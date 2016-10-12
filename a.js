4c50d75a1db2b13156a6bd4258dfe9ec64ebd90d
gxc213 <891570685@qq.com>
2016-10-12 20:35:02 +0800

    '增加v2'

--- a.js
+++ a.js
@@ -2,12 +2,16 @@
 * @Author: gxc213
 * @Date:   2016-10-12 08:53:17
 * @Last Modified by:   gxc213
-* @Last Modified time: 2016-10-12 20:20:58
+* @Last Modified time: 2016-10-12 20:34:14
 */
 
 'use strict';
 var a=2;
 $(function(){
+//v5
+var c=13;
+var d=23;
 
-
+//v2
+var g=123;
 })
