77b0aaa24917561f2f619669a754835fe32eb55a
gxc213 <891570685@qq.com>
2016-10-12 20:15:42 +0800

    '提交git1和js'

---
+++ a.js
@@ -0,0 +1,15 @@
+/*
+* @Author: gxc213
+* @Date:   2016-10-12 08:53:17
+* @Last Modified by:   gxc213
+* @Last Modified time: 2016-10-12 20:14:20
+*/
+
+'use strict';
+var a=2;
+$(function(){
+
+//v5
+var c=23;
+var d=13;
+})
