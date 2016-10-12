7b2d7c66c53329b326f995452551871b8efee95e
gxc213 <891570685@qq.com>
2016-10-12 20:22:41 +0800

    '改变了js'

--- a.js
+++ a.js
@@ -2,14 +2,12 @@
 * @Author: gxc213
 * @Date:   2016-10-12 08:53:17
 * @Last Modified by:   gxc213
-* @Last Modified time: 2016-10-12 20:14:20
+* @Last Modified time: 2016-10-12 20:20:58
 */
 
 'use strict';
 var a=2;
 $(function(){
 
-//v5
-var c=23;
-var d=13;
+
 })
